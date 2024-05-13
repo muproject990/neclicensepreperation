d738751c6418a1cce767f64bebbf5a694e1b938f
Aavash Paudel <muproject990@gmail.com>
2024-05-14 00:55:26 +0545

    sign up sign navigation error

--- lib/features/auth/presentation/signup_page.dart
+++ lib/features/auth/presentation/signup_page.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:neclicensepreperation/core/app_pallete.dart';
+import 'package:neclicensepreperation/features/auth/presentation/login_page.dart';
 import 'package:neclicensepreperation/features/auth/presentation/widgets/auth_feild.dart';
 import 'package:neclicensepreperation/features/auth/presentation/widgets/auth_gradient_button.dart';
 
 class SignUpPage extends StatefulWidget {
+  static route() => MaterialPageRoute(
+        builder: (context) => const SignUpPage(),
+      );
   const SignUpPage({super.key});
 
   @override
@@ -73,19 +77,27 @@ class _SignUpPageState extends State<SignUpPage> {
               const SizedBox(
                 height: 20,
               ),
-              RichText(
-                text: TextSpan(
-                  text: "Already have an account ? ",
-                  style: Theme.of(context).textTheme.titleMedium,
-                  children: [
-                    TextSpan(
-                      text: "Sign In",
-                      style: Theme.of(context)
-                          .textTheme
-                          .titleMedium
-                          ?.copyWith(color: AppPallete.gradient2),
-                    ),
-                  ],
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    LoginPage.route(),
+                  );
+                },
+                child: RichText(
+                  text: TextSpan(
+                    text: "Already have an account ? ",
+                    style: Theme.of(context).textTheme.titleMedium,
+                    children: [
+                      TextSpan(
+                        text: "Sign In",
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleMedium
+                            ?.copyWith(color: AppPallete.gradient2),
+                      ),
+                    ],
+                  ),
                 ),
               )
             ],
