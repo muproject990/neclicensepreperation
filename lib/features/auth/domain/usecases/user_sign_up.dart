e1f63dfef5b7ddd03603ea2ea9ad62b828b4eb35
Aavash Paudel <muproject990@gmail.com>
2024-05-13 23:57:40 +0545

    user sign in vayo  yesma ni yo independent x aaaru model sanga

---
+++ lib/features/auth/domain/usecases/user_sign_up.dart
@@ -0,0 +1,32 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:neclicensepreperation/core/error/failure.dart';
+import 'package:neclicensepreperation/core/usecase/usercase.dart';
+import 'package:neclicensepreperation/features/auth/domain/auth-repository.dart';
+
+class UserSignUp implements UseCase<String, UserSignUpParams> {
+  final AuthRepository authRepository;
+  
+  const UserSignUp({required this.authRepository});
+
+  @override
+  Future<Either<Failure, String>> call(UserSignUpParams params) async {
+    // dont forget to use await becoz sometimes it will takes some time
+    return await authRepository.signUpWithEmailPassword(
+      name: params.name,
+      email: params.email,
+      password: params.password,
+    );
+  }
+}
+
+class UserSignUpParams {
+  final String email;
+  final String password;
+  final String name;
+
+  UserSignUpParams({
+    required this.email,
+    required this.password,
+    required this.name,
+  });
+}
