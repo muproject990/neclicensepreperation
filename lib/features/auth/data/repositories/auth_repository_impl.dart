b241256e1991643e8d5b30a7db53027cf8e269ee
Aavash Paudel <muproject990@gmail.com>
2024-05-13 23:38:59 +0545

    yo chani  implementation jo chahi only data linklayer ma depend xa and seperate xa domain bata

---
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -0,0 +1,37 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:neclicensepreperation/core/error/exception.dart';
+import 'package:neclicensepreperation/core/error/failure.dart';
+import 'package:neclicensepreperation/features/auth/data/datasource/auth_remote_data_source.dart';
+import 'package:neclicensepreperation/features/auth/domain/auth-repository.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthRemoteDataSource remoteDataSource;
+
+  const AuthRepositoryImpl(this.remoteDataSource);
+
+  @override
+  Future<Either<Failure, String>> loginWithEmailPassword(
+      {required String email, required String password}) {
+    // TODO: implement loginWithEmailPassword
+    throw UnimplementedError();
+  }
+
+// ! signUpWithEmailPassword
+  @override
+  Future<Either<Failure, String>> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final userId = await remoteDataSource.signUpWithEmailPassword(
+        name: name,
+        email: email,
+        password: password,
+      );
+      return right(userId);
+    } on ServerException catch (e) {
+      return left(Failure(e.message));
+    }
+  }
+}
