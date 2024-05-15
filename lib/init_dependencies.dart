05796e88d27586116d84d47725d50a2d18a26cfd
Aavash Paudel <muproject990@gmail.com>
2024-05-15 12:34:58 +0545

    get ir impplemented sucessfully

---
+++ lib/init_dependencies.dart
@@ -0,0 +1,64 @@
+import 'package:get_it/get_it.dart';
+import 'package:neclicensepreperation/core/secrets/supabase_secret.dart';
+import 'package:neclicensepreperation/features/auth/data/datasource/auth_remote_data_source.dart';
+import 'package:neclicensepreperation/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:neclicensepreperation/features/auth/domain/auth-repository.dart';
+import 'package:neclicensepreperation/features/auth/domain/usecases/user_sign_up.dart';
+import 'package:neclicensepreperation/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+final serviceLocator = GetIt.instance;
+
+Future<void> initDependencies() async {
+  _initAuth();
+  final supabase = await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnonKey,
+  );
+  serviceLocator.registerLazySingleton(() => supabase.client);
+}
+
+void _initAuth() {
+  serviceLocator
+    // core
+    // ..registerLazySingleton(
+    //   () => AppUserCubit(),
+    // )
+    // DataSource
+    ..registerFactory<AuthRemoteDataSource>(
+      () => AuthRemoteDataSourceImpl(
+        serviceLocator<SupabaseClient>(),
+      ),
+    )
+    // Repository
+    ..registerFactory<AuthRepository>(
+      () => AuthRepositoryImpl(
+        serviceLocator(),
+      ),
+    )
+    // Usecases
+    ..registerFactory(
+      () => UserSignUp(
+        serviceLocator(),
+      ),
+    )
+    // ..registerFactory(
+    //   () => UserLogin(
+    //     serviceLocator(),
+    //   ),
+    // )
+    // ..registerFactory(
+    //   () => CurrentUser(
+    //     serviceLocator(),
+    //   ),
+    // )
+    // Block
+    ..registerLazySingleton(
+      () => AuthBloc(
+        userSignUp: serviceLocator(),
+        // userLogin: serviceLocator(),
+        // currentUser: serviceLocator(),
+        // appUserCubit: serviceLocator(),
+      ),
+    );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:neclicensepreperation/core/secrets/supabase_secret.dart';
 import 'package:neclicensepreperation/core/theme.dart';
 import 'package:neclicensepreperation/features/auth/data/datasource/auth_remote_data_source.dart';
 import 'package:neclicensepreperation/features/auth/data/repositories/auth_repository_impl.dart';
 import 'package:neclicensepreperation/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:neclicensepreperation/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:neclicensepreperation/features/auth/presentation/signup_page.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:neclicensepreperation/init_dependencies.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  final supabase = await Supabase.initialize(
-    url: AppSecrets.supabaseUrl,
-    anonKey: AppSecrets.supabaseAnonKey,
-  );
+  await initDependencies();
 
   runApp(MultiBlocProvider(
     providers: [
       BlocProvider(
-        create: (_) => AuthBloc(
-          userSignUp: UserSignUp(
-            AuthRepositoryImpl(
-              AuthRemoteDataSourceImpl(
-                supabase.client,
-              ),
-            ),
-          ),
-        ),
-      )
+        create: (_) => serviceLocator<AuthBloc>(),
+      ),
     ],
     child: const MyApp(),
   ));
