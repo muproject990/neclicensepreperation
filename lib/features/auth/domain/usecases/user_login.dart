36965f5241d25d030684a9ab8f1307d01f839602
Aavash Paudel <muproject990@gmail.com>
2024-05-15 16:05:22 +0545

    login usecase created sucessfully

---
+++ lib/features/auth/domain/usecases/user_login.dart
@@ -0,0 +1,23 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:neclicensepreperation/core/error/failure.dart';
+import 'package:neclicensepreperation/core/usecase/usercase.dart';
+import 'package:neclicensepreperation/features/auth/domain/auth-repository.dart';
+import 'package:neclicensepreperation/features/auth/domain/entities/user.dart';
+
+class UserLogin implements UseCase<User, UserLoginParams> {
+  final AuthRepository authRepository;
+  const UserLogin(this.authRepository);
+
+  @override
+  Future<Either<Failure, User>> call(UserLoginParams params) {
+    return authRepository.loginWithEmailPassword(
+        email: params.email, password: params.password);
+  }
+}
+
+class UserLoginParams {
+  final String email;
+  final String password;
+
+  UserLoginParams({required this.email, required this.password});
+}
