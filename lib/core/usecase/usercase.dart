9900c6351db90d1ead34f2875b30529f2a91263b
Aavash Paudel <muproject990@gmail.com>
2024-05-21 19:48:13 +0545

    1.created NoParams Class for taking  no prams 2.created current which implement  along with 1 3.registered in Auth Block to state management using Bloc 4.  an class AuthIsUserLoggedIn is created so that i can bes used in 3

--- lib/core/usecase/usercase.dart
+++ lib/core/usecase/usercase.dart
@@ -4,3 +4,5 @@ import 'package:neclicensepreperation/core/error/failure.dart';
 abstract interface class UseCase<SuccessType, Params> {
   Future<Either<Failure, SuccessType>> call(Params params);
 }
+
+class NoParams {}
--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:neclicensepreperation/core/usecase/usercase.dart';
 import 'package:neclicensepreperation/features/auth/domain/entities/user.dart';
+import 'package:neclicensepreperation/features/auth/domain/usecases/current_user.dart';
 import 'package:neclicensepreperation/features/auth/domain/usecases/user_login.dart';
 import 'package:neclicensepreperation/features/auth/domain/usecases/user_sign_up.dart';
 
@@ -10,17 +12,35 @@ part 'auth_state.dart';
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final UserSignUp _userSignUp;
   final UserLogin _userLogin;
+  final CurrentUser _currentUser;
   AuthBloc({
     required UserLogin userLogin,
     required UserSignUp userSignUp,
+    required CurrentUser currentUser,
   })  : _userSignUp = userSignUp,
         _userLogin = userLogin,
+        _currentUser = currentUser,
         super(AuthInitial()) {
     on<AuthSignUp>(_onAuthSighUp);
     on<AuthLogin>(_onAuthLogin);
+    on<AuthIsUserLoggedIn>(_isUserLoggedIn);
   }
 
-  void _onAuthSighUp(AuthSignUp event, Emitter<AuthState> emit) async {
+  void _isUserLoggedIn(
+    AuthIsUserLoggedIn event,
+    Emitter<AuthState> emit,
+  ) async {
+    final res = await _currentUser(NoParams());
+    res.fold(
+      (l) => emit(AuthFailure(l.message)),
+      (r) => emit(AuthSuccess(user: r)),
+    );
+  }
+
+  void _onAuthSighUp(
+    AuthSignUp event,
+    Emitter<AuthState> emit,
+  ) async {
     emit(AuthLoading());
     final response = await _userSignUp(
       UserSignUpParams(
@@ -38,7 +58,10 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     );
   }
 
-  void _onAuthLogin(AuthLogin event, Emitter<AuthState> emit) async {
+  void _onAuthLogin(
+    AuthLogin event,
+    Emitter<AuthState> emit,
+  ) async {
     emit(AuthLoading());
 
     final res = await _userLogin(UserLoginParams(
