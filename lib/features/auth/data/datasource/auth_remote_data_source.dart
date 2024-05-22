a794307cf39aa86f13ee97591bc99f84289d50e9
Aavash Paudel <muproject990@gmail.com>
2024-05-22 18:57:56 +0545

    implemented copywith in login and signup

--- lib/features/auth/data/datasource/auth_remote_data_source.dart
+++ lib/features/auth/data/datasource/auth_remote_data_source.dart
@@ -51,6 +51,8 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
       // print(response.user!.toJson());
       return UserModel.fromJson(
         response.user!.toJson(),
+      ).copyWith(
+        email: currentUserSession!.user.email,
       );
     } catch (e) {
       throw ServerException(message: e.toString());
@@ -81,8 +83,8 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
         throw ServerException(message: "User is nulll");
       }
       print(response.user!.toJson());
-      return UserModel.fromJson(
-        response.user!.toJson(),
+      return UserModel.fromJson(response.user!.toJson()).copyWith(
+        email: currentUserSession!.user.email,
       );
     } catch (e) {
       throw ServerException(message: e.toString());
